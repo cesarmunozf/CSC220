e4818d839c21236b406fbc00455a345fe9b5ef63
cesarmunozf <CES2022125@maricopa.edu>
2017-07-12 19:54:14 -0700

    second version

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){
-   int x = 34;
    int *y = &x;
    int **z = &x;
    printf("z=%p, *z=%p, **z= %d &z=%p\n", z, *z, **z, &z); 
+   printf("An extra message");
    return 0; 
 }
